bddf71e9f3ad01586bb720887d70cf9a07fab6f6
GohEeEn <34720382+GohEeEn@users.noreply.github.com>
2021-05-18 16:40:02 +0100

    Enable password policy
    - Password policy defined in RegexConstant class as a constant
    - Refactor client-side password strength visualizer code
    - Improve client-side show password functionality

--- src/main/java/ucd/comp40660/validator/UserValidator.java
+++ src/main/java/ucd/comp40660/validator/UserValidator.java
@@ -11,24 +11,11 @@ import ucd.comp40660.user.model.User;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static ucd.comp40660.filter.RegexConstants.NAME_REGEX;
+import static ucd.comp40660.filter.RegexConstants.PASSWORD_REGEX;
+
 @Component
 public class UserValidator implements Validator {
-
-    private final String NAME_REGEX = "([A-Za-z]*).{2,32}";
-
-    /**
-     * (?=.*[a-z])   The string must contain at least 1 lowercase alphabetical character
-     * (?=.*[A-Z])	The string must contain at least 1 uppercase alphabetical character
-     * (?=.*[0-9])	The string must contain at least 1 numeric character
-     * (?=.*[!@#$%^&*])	The string must contain at least one special character, but we are escaping reserved RegEx characters to avoid conflict
-     * (?=.{8,32})	The string must be eight characters or longer up to 32 characters
-     */
-    private final String PASSWORD_REGEX = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!]).{8,32})";
-
-    /**
-     * Reference : https://owasp.org/www-community/OWASP_Validation_Regex_Repository
-     */
-    private final String CREDIT_CARD_REGEX = "((4\\d{3})|(5[1-5]\\d{2})|(6011)|(7\\d{3}))-?\\d{4}-?\\d{4}-?\\d{4}|3[4,7]\\d{13}";
 
     @Autowired
     private UserService userService;
--- src/main/resources/static/js/show_password.js
+++ src/main/resources/static/js/show_password.js
@@ -1,10 +1,13 @@
 function showPassword() {
 
     const x = document.getElementById("password");
+    const y = document.getElementById("passwordConfirm");
 
-    if (x.type === "password") {
-    x.type = "text";
+    if (x.type === "password" || y.type === "password") {
+        x.type = "text";
+        y.type = "text";
     } else {
         x.type = "password";
+        y.type = "password";
     }
 }
