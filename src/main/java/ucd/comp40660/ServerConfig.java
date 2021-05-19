4aec39ff1ca6360b73c7a174f5764572897ee883
GohEeEn <34720382+GohEeEn@users.noreply.github.com>
2021-05-19 18:08:13 +0100

    Enable redirection of HTTP traffic to HTTPS

---
+++ src/main/java/ucd/comp40660/ServerConfig.java
@@ -0,0 +1,49 @@
+package ucd.comp40660;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+
+    /**
+     * Configure the Tomcat web server used on spring boot 2.x
+     * to enable HTTP traffic redirection to HTTPS
+     */
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    /**
+     * Redirect HTTP request to Tomcat's HTTPS port (8443)
+     * Reference : https://stackoverflow.com/questions/61760786/what-is-the-use-of-port-8443
+     * @return
+     */
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);    // Port 8443 in Apache Tomcat is used for running your service at HTTPS
+        return connector;
+    }
+}
